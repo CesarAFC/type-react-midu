fd19ddcee93723e27cc3ba61ccaf13e92f768377
Cesar Fontalvo <cafc2030@gmail.com>
2023-06-07 11:08:53 -0500

    adding interfaces

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
+
+interface Sub {
+  nick: string
+  subMonths: number
+  avatar: string
+  description?: string
+}
+
+interface AppState {
+  subs: Array<Sub>
+}
+
+const initialState = [{
+  nick: 'dapelu',
+  subMonths: 3,
+  avatar: 'https://i.pravatar.cc/150?u=dapelu',
+  description: 'Dapelu hace de moderador',
+},
+{
+  nick: 'cesar_fontalvo',
+  subMonths: 7,
+  avatar: 'https://i.pravatar.cc/150?u=cesar_fontalvo',
+}]
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [subs, setSubs] = useState<AppState['subs']>([]);
+
+  useEffect(() => {
+    setSubs(initialState)
+  }, [])
+  
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Cesar Subs</h1>
+      <ul>
+        {
+        subs.map( sub => {
+          return (
+            <li key={sub.nick}>
+              <img src={sub.avatar} alt={`Avatar for ${sub.nick}`} />
+              <h4>{sub.nick} (<small>{sub.subMonths}</small>)</h4>
+              <p>{sub.description?.substring(0, 100)}</p>
+            </li>
+          )
+        })
+        }
+      </ul>
     </>
   )
 }
